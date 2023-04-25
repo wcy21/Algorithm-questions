f69d6593272994f63b34377e6dfffc40d2caf8b3
blue dog <72981121+wcy21@users.noreply.github.com>
2023-04-25 16:07:55 +0800

    Update AcWing-4_多重背包问题I.cpp
    https://www.acwing.com/problem/content/4/

--- 背包dp/AcWing-4_多重背包问题I.cpp
+++ 背包dp/AcWing-4_多重背包问题I.cpp
@@ -4,30 +4,20 @@ using namespace std;
 
 const int SIZE = 1007;
 
-struct node {
-    int w, v;
-} l[SIZE];
-int n, W, idx = 0;
+int n, W;
+int w[SIZE], v[SIZE], c[SIZE];
 int f[SIZE];
 
 int main() {
     cin >> n >> W;
-    for (int i = 0; i < n; ++i) {
-        int k = 1, ci, wi, vi;
-        cin >> wi >> vi >> ci;
-        while (k < ci) {
-            l[idx].w = k * wi;
-            l[idx++].v = k * vi;
-            ci -= k;
-            k <<= 1;
-        }
-        l[idx].w = ci * wi;
-        l[idx++].v = ci * vi;
-    }
+    for (int i = 0; i < n; ++i)
+        cin >> w[i] >> v[i] >> c[i];
 
-    for (int i = 0; i < idx; ++i)
-        for (int j = W; j >= l[i].w; --j) 
-            f[j] = max(f[j], f[j - l[i].w] + l[i].v);
+    for (int i = 0; i < n; ++i)
+        for (int j = W; j >= w[i]; --j)
+            for (int k = 0; k <= c[i]; ++k)
+                if (j >= k * w[i])
+                    f[j] = max(f[j], f[j - w[i] * k] + v[i] * k);
 
     cout << f[W] << endl;
 
